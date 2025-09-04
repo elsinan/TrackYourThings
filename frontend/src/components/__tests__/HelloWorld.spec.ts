c6f63d61e0603d886ec553ad1bc63ec731a082c3
Sinan Elmas <sinan.elmas@gmx.net>
2025-09-04 18:24:04 +0200

    fix tests

--- frontend/src/components/__tests__/HelloWorld.spec.ts
+++ frontend/src/components/__tests__/HelloWorld.spec.ts
@@ -1,11 +1,11 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+import CalenderItem from '../CalendarItem.vue'
 
 describe('HelloWorld', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
+    const wrapper = mount(CalenderItem)
+    expect(wrapper.text()).toContain('Kalender')
   })
 })
